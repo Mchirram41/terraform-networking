c2293ecbb89d515948ed93b025f9054bd76bd06a
Mchirram41 <mchirram41@gmail.com>
2026-07-05 19:15:15 +0530

    Refactor RDS variable definitions with types and descriptions
    Updated variable definitions to include types and descriptions for better clarity and usability.

--- new-tr/networking/modules/RDS/variable.tf
+++ new-tr/networking/modules/RDS/variable.tf
@@ -1,48 +1,85 @@
-variable "identifier" {}
-
-variable "db_name" {}
-variable "username" {}
-variable "password" {}
-
-variable "vpc_id" {}
-
-variable "subnet_ids" {
-  type = list(string)
+variable "identifier" {
+  type        = string
+  description = "RDS instance identifier"
 }
 
-# existing SG from VPC/app module
-variable "allowed_security_group_id" {}
+variable "db_name" {
+  type        = string
+  description = "Initial database name"
+}
+
+variable "username" {
+  type        = string
+  description = "Master username for RDS"
+}
+
+variable "password" {
+  type        = string
+  sensitive   = true
+  description = "Master password for RDS"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "List of subnet IDs for RDS subnet group"
+}
+
+variable "allowed_security_group_id" {
+  type        = string
+  description = "Security group ID to allow RDS access"
+}
 
 variable "multi_az" {
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
+  description = "Enable Multi-AZ deployment"
 }
 
 variable "engine" {
-  default = "mysql"
+  type        = string
+  default     = "mysql"
+  description = "Database engine (mysql, postgres, etc)"
 }
 
 variable "engine_version" {
-  default = "8.0"
+  type        = string
+  default     = "8.0"
+  description = "Engine version"
 }
 
 variable "instance_class" {
-  default = "db.t3.micro"
+  type        = string
+  default     = "db.t3.micro"
+  description = "Instance class size"
 }
 
 variable "allocated_storage" {
-  default = 20
+  type        = number
+  default     = 20
+  description = "Allocated storage in GB"
+}
+
+variable "resource_name" {
+  type        = string
+  description = "Resource name prefix"
 }
 
 variable "project_name" {
-  type = string
+  type        = string
+  description = "Project name"
 }
 
 variable "environment" {
-  type = string
+  type        = string
+  description = "Environment name (dev, prod, etc)"
 }
 
 variable "tags" {
-  type = map(string)
+  type        = map(string)
+  description = "Common tags"
 }
-
